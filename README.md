23cee1fa723fec5dc9482683b49b3acf7ea9d1e8
Marc McIntosh <email@marcmcintosh.ninja>
2016-10-14 20:49:08 +0000

    Changes to be committed: 	modified:   README.md 	modified:   package.json 	modified:   server.js

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # imagesearch-api
+written in response to <https://www.freecodecamp.com/challenges/image-search-abstraction-layer>
+basically provides an abstraction layer over google-images
+
+## Usage
+Requires mongodb to running
+and a custom google-search-engine setup.
+see <https://www.npmjs.com/package/google-images> for instructions on how to do this 
+```bash
+  node server.js
+```
+and in other terminal
+```bash
+ curl http://0.0.0.0:8080/search/lolcats
+ # Will perform the search
+ # and to check the most recent searches
+ curl http://0.0.0.0:8080/latest
+```
+ 
--- server.js
+++ server.js
@@ -1,5 +1,83 @@
 "use strict";
+const url = require('url');
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const request = require('request');
 require('dotenv').config();
-const ImagesClient = require('google-images');
-let client = new ImagesClient('CSE ID', process.env.CSE_ID );
+
+function saveSearch(term, cb){
+  MongoClient.connect(process.env.MONGO, (err,db)=>{
+   if(err){
+      cb(err);
+    } else {
+      const collection = db.collection('searches');
+      collection.insert({term: term, when: Date.now()},(err, item)=>{
+        if(err) { cb(err) } else { cb(null) }
+        db.close();
+      });
+    }
+  });
+};
+
+function latestSearches(cb){
+  MongoClient.connect(process.env.MONGO, (err, db)=>{
+   if(err) { cb(err) } else {
+     const collection = db.collection('searches');
+     collection.find({},{
+       _id: 0, term: 1, when: 1
+     }).sort({ when: -1 }).limit(20).toArray((err, docs)=>{
+       if(err) { cb(err) } else {
+         cb(null, docs);
+       }
+       db.close();
+     });
+   }
+  });
+}
+
+function mkSearchString(term, offset){
+  return url.format({
+    protocol: 'https:',
+    slashes: true,
+    host: 'www.googleapis.com',
+    query: { 
+      q: (term) ? term : 'lolcats',
+      start: (offset > 0 ) ? offset : 1,  
+      cx: process.env.CSE_ID,
+      key: process.env.API_KEY
+    },
+    pathname: '/customsearch/v1',
+  });
+}
+
+const app = express();
+
+app.get('/latest', (req, res)=>{
+   latestSearches((err, docs)=>{
+    if(err) {
+      res.status(500).json(err);
+    } else {
+      res.json(docs);
+    }
+   });
+});
+
+app.get('/search/:term', (req, res)=>{
+  const search = mkSearchString(req.params.term, req.query);
+  //request.get({uri: search, json:true}).on('error',(err)=>{console.error(err)}).pipe(res);  
+  request.get({uri: search, json:true}, (err, resp, body)=>{
+    //res.writeHead({'Content-Type': 'application/json'});
+    if (err) {
+      res.end(err)
+    } else if (!err && resp.statusCode == 200) {
+      saveSearch(req.params.term,(err)=>{
+        if(err) { console.error(err); }
+        res.end(JSON.stringify(body.items));
+      });
+    } else {
+      res.end(JSON.stringify(resp));
+   }
+  });  
+});
+app.listen(8080);
 
